56a79105c2054ca0bfecbe768aa0ec88481d430e
inalbon <malika.in-albon@epfl.ch>
2021-05-06 16:34:52 +0200

    Linear and rotation set speed functions

--- move.c
+++ move.c
@@ -8,3 +8,29 @@
 #include <obstacle_detection.h>
 #include <pi_regulator.h>
 #include <motors.h>
+
+#define NORMAL_SPEED 350; // step/s
+#define SPEED_MAX 350; //step/s
+
+int16_t set_speed_rot(bool obstacle, int16_t speed_pi){
+	int16_t speed = 0;
+
+	if(obstacle)
+		speed = 0;
+	else
+		speed = speed_pi;
+
+	return speed;
+}
+
+int16_t set_speed_lin(bool obstacle){
+	int16_t speed = 0;
+	if(obstacle)
+			speed = 0;
+	else
+		speed = NORMAL_SPEED;
+
+	return speed;
+}
+
+
