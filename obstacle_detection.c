1b58a1e530b1b1659a73749483691c937ff52c70
inalbon <malika.in-albon@epfl.ch>
2021-05-06 15:18:25 +0200

    Obstacle detection function

--- obstacle_detection.c
+++ obstacle_detection.c
@@ -6,5 +6,27 @@
 
 #include <sensors/proximity.h>
 
+//IR0 (front-right)
+//IR1 (front-right-45deg)
+//IR7 (front-left)
+//IR6 (front-left-45deg)
+#define IR0 0, IR1 1, IR2 2, IR3 3, IR4 4, IR5 5, IR6 6, IR7 7
+#define DELTA_MAX 300 //difference between ambient and reflected light
 
+bool obstacle_detection(void){
+	uint16_t i = 0;
+	bool obstacle = 0;
+	int16_t delta[PROXIMITY_NB_CHANNELS] = {0};
+
+	for(i=0; i<PROXIMITY_NB_CHANNELS; i++){
+		delta[i] = get_prox(i);
+
+		if (delta[i] > DELTA_MAX)
+			obstacle = 1;
+	}
+
+
+
+	return obstacle;
+}
 
