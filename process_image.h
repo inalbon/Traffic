12c4471d50104628884f978b06c5c6f1ee373149
Isione <isione.bonvalot@epfl.ch>
2021-05-07 14:06:53 +0200

    float get_offset_from_center() + deleted unused arguments in

--- process_image.h
+++ process_image.h
@@ -1,7 +1,7 @@
 #ifndef PROCESS_IMAGE_H
 #define PROCESS_IMAGE_H
 
-int32_t get_offset_from_center(uint8_t *buffer);
+float get_offset_from_center(void);
 void process_image_start(void);
 
 #endif /* PROCESS_IMAGE_H */
