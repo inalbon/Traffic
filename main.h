13c7f120a2b7b755549b24f2172bee1b4c1eba2e
Isione <isione.bonvalot@epfl.ch>
2021-05-07 14:14:16 +0200

    New value ROTATION_MAX = 125

--- main.h
+++ main.h
@@ -15,7 +15,7 @@ extern "C" {
 #define WIDTH_SLOPE				5
 #define MIN_LINE_WIDTH			40
 #define ROTATION_THRESHOLD		10
-#define ROTATION_MAX			50 
+#define ROTATION_MAX			125 
 #define ROTATION_COEFF			2 
 #define MAX_DISTANCE 			25.0f
 #define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
