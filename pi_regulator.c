80c908865d3e58afd3d238d2f39fe549f1f427e5
Isione <isione.bonvalot@epfl.ch>
2021-05-07 13:51:24 +0200

    pi_regulator() completed

--- pi_regulator.c
+++ pi_regulator.c
@@ -14,6 +14,31 @@
 
 //simple PI regulator implementation
 int16_t pi_regulator(float error){
+    
+    float speed = 0;
+
+    static float sum_error = 0;
+
+    //disables the PI regulator if the error is to small
+    //this avoids to always move as we cannot exactly be where we want and 
+    //the camera is a bit noisy
+    if(fabs(error) < ERROR_THRESHOLD){
+        return 0;
+    }
+
+    sum_error += error;
+
+    //we set a maximum and a minimum for the sum to avoid an uncontrolled growth
+    if(sum_error > MAX_SUM_ERROR){
+        sum_error = MAX_SUM_ERROR;
+    }else if(sum_error < -MAX_SUM_ERROR){
+        sum_error = -MAX_SUM_ERROR;
+    }
+
+    speed = KP * error + KI * sum_error;
+    //chprintf((BaseSequentialStream *)&SD3, "error = %f \n",error);
+
+    return (int16_t)speed;
 
 
 }
