b0cbc2ea72b7875753d67f95824e6b4f7c071091
Isione <isione.bonvalot@epfl.ch>
2021-05-16 11:23:16 +0200

    Update comment pi_regulator

--- pi_regulator.c
+++ pi_regulator.c
@@ -14,32 +14,29 @@
 int16_t pi_regulator(int16_t error){
 
     float speed = 0;
-
     static int16_t sum_error = 0;
 
     //disables the PI regulator if the error is to small
     //this avoids to always rotate the robot as we cannot be exactly on the line and
     //the camera is a bit noisy
-    if(fabs(error) < ERROR_THRESHOLD){
+    if(fabs(error) < ERROR_THRESHOLD)
         return 0;
-    }
 
-    sum_error += error;
+    sum_error += error; 
+
     //chprintf((BaseSequentialStream*)&SD3, "\n sum error =  %d \n ", sum_error);
 
     //we set a maximum and a minimum for the sum to avoid an uncontrolled growth
-    if(sum_error > MAX_SUM_ERROR){
+    if(sum_error > MAX_SUM_ERROR)
         sum_error = MAX_SUM_ERROR;
-    }else if(sum_error < -MAX_SUM_ERROR){
+    else if(sum_error < -MAX_SUM_ERROR)
         sum_error = -MAX_SUM_ERROR;
-    }
 
-    speed = KP * error + KI * sum_error;
+    speed = KP * error + KI * sum_error; //formula for a PI regulator
 
     //chprintf((BaseSequentialStream *)&SD3, "speed = %f \n",speed);
 
     return (int16_t)speed;
-
 }
 
 int16_t get_speed_pi(void){
@@ -48,5 +45,6 @@ int16_t get_speed_pi(void){
 
     //computes a rotation to let the robot rotate to be on the line
     speed_rotation = pi_regulator(get_offset_from_center());
+
     return speed_rotation;
 }
