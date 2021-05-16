2d5a6da26880e3ae0b0440a97a69a051febfb46d
Isione <isione.bonvalot@epfl.ch>
2021-05-16 10:04:49 +0200

    Comment header process_image

--- process_image.h
+++ process_image.h
@@ -1,3 +1,15 @@
+/*
+File    : process_image.h
+Author  : Isione Bonvalot
+Date    : 6 May 2021
+REV 1.0
+
+Functions to capture and process images from the camera PO830D
+
+Adapted from the code of TP4 CamReg
+taken at https://moodle.epfl.ch/course/view.php?id=467
+*/
+
 #ifndef PROCESS_IMAGE_H
 #define PROCESS_IMAGE_H
 
